c972c11b84f24591d9ba7bfa343e67935f3d16a7
Worme Benjamin <benjamin.worme@etudiant.univ-lille1.fr>
2018-03-23 09:16:20 +0100

    l'affichage relié a la base de donné des exo

--- server/src/main/webapp/exercer.js
+++ server/src/main/webapp/exercer.js
@@ -4,5 +4,61 @@ $(document).ready(function(){
     	$(".pExercice").show();
     	$(".pEspace").hide();
     	console.log("test");
-    }); 
+        $.ajax({
+            url: "http://localhost:8080/cye/exercice",
+            type: "GET",
+            contentType: 'application/json;charset=utf-8',
+
+            success: function(json) {
+    		$("#formOutPut").empty()
+    		console.log("success");
+			
+              	json.forEach(function(exercice, idx) {
+		    var btn = document.createElement("BUTTON");
+		    var t = document.createTextNode(""+exercice.nom);
+		//	var p = document.createTextNode(""+exercice.
+		    btn.appendChild(t);
+			btn.className = "buttonExo";
+			btn.id = ""+exercice.id;
+		    $("#formOutPut").append(btn);
+			$("#formOutPut").append("<br>");
+    		});
+	    },
+
+	    error: function(xhr, status, errorThrown){
+                alert("Problem");
+                console.log( "Error: " + errorThrown );
+                console.log( "Status: " + status );
+	    }
+        });
+    });
 });
+	
+	/*$(".buttonExo").click(function(){
+    	$(".pRessource").hide();
+    	$(".pExercice").show();
+    	$(".pEspace").hide();
+    	console.log("test");
+        $.ajax({
+            url: "http://localhost:8080/cye/exercice",
+            type: "GET",
+            contentType: 'application/json;charset=utf-8',
+
+            success: function(json) {
+    		$("#formOutPut").empty()
+    		console.log("success");
+			
+              	json.forEach(function(exercice, idx) {
+		    var btn = document.createElement("BUTTON");
+		    var t = document.createTextNode(""+exercice.nom);
+			var f = document.create
+		    btn.appendChild(t);
+			btn.className = "buttonExo";
+			btn.id = ""+exercice.id;
+		    $("#formOutPut").append(btn);
+			$("#formOutPut").append("<br>");
+    		});
+	    },*/
+
+
+// http://hevea22:8080/cye/question/exoid/1 A UTILISER
