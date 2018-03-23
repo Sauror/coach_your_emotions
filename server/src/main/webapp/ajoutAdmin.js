e1b24c9b047cb86b151938251e2cc1dfce94effc
Antoine Turostowski <antoine.turostowski@etudiant.univ-lille1.fr>
2018-03-23 17:31:30 +0100

    OK NOW

--- server/src/main/webapp/ajoutAdmin.js
+++ server/src/main/webapp/ajoutAdmin.js
@@ -1,211 +1,210 @@
 $(document).ready(function(){
-    $("#AjoutAdmin").click(function(){
-    	$(".pRessource").hide();
-    	$(".pExercice").hide();
-    	$(".pEspace").hide();
-	$(".pAjoutAdmin").show();
-	$(".pAjoutSentiment").hide();
-	$(".pAjoutBesoin").hide();
-	$(".pAjoutExercice").hide();
-	$(".pAjoutQuestion").hide();
-	$(".pLogin").hide();
-	$(".pSign").hide();
-    });
-
-    $("#AjoutSentiment").click(function(){
-	$(".pRessource").hide();
-	$(".pExercice").hide();
-	$(".pEspace").hide();
-	$(".pAjoutAdmin").hide();
-	$(".pAjoutSentiment").show();
-	$(".pAjoutBesoin").hide();
-	$(".pAjoutExercice").hide();
-	$(".pAjoutQuestion").hide();
-	//$("#sentiment").hide();
-	$(".pLogin").hide();
-	$(".pSign").hide();
-    });
-
-    $("#AjoutBesoin").click(function(){
-	$(".pRessource").hide();
-	$(".pExercice").hide();
-	$(".pEspace").hide();
-	$(".pAjoutAdmin").hide();
-	$(".pAjoutSentiment").hide();
-	$(".pAjoutBesoin").show();
-	$(".pAjoutExercice").hide();
-	$(".pAjoutQuestion").hide();
-	$(".pLogin").hide();
-	$(".pSign").hide();
-    });
-
-    $("#AjoutExercice").click(function(){
-	$(".pRessource").hide();
-	$(".pExercice").hide();
-	$(".pEspace").hide();
-	$(".pAjoutAdmin").hide();
-	$(".pAjoutSentiment").hide();
-	$(".pAjoutBesoin").hide();
-	$(".pAjoutExercice").show();
-	$(".pAjoutQuestion").hide();
-	$(".pLogin").hide();
-	$(".pSign").hide();
-    });
-
-    $("#AjoutQuestion").click(function(){
-	$(".pRessource").hide();
-	$(".pExercice").hide();
-	$(".pEspace").hide();
-	$(".pAjoutAdmin").hide();
-	$(".pAjoutSentiment").hide();
-	$(".pAjoutBesoin").hide();
-	$(".pAjoutExercice").hide();
-	$(".pAjoutQuestion").show();
-	$(".pLogin").hide();
-	$(".pSign").hide();
-    });
-
-    $("#postSentiment").click(function(){
-	console.log($('#categorieSentiment').val())
-	console.log($('#nomSentiment').val())
-	postSentiment(
-	    $('#nomSentiment').val(),
-	    $('#categorieSentiment').val(),
-	    'http://localhost:8080/cye/sentiment'
-	)});
-
-    function postSentiment(sentiment,categorie,url) {
-	console.log("postSentiment " + url);
-	$.ajax({
-	    type : 'POST',
-	    contentType : 'application/json',
-	    url : url,
-	    dataType : "json",
-	    data : JSON.stringify({
-		"nom" : sentiment,
-		"categorie" : categorie,
-		"id" : 0
-	    }),
-	    success : function(data, textStatus, jqXHR) {
-		//afficheUser(data);
-	    },
-	    error : function(jqXHR, textStatus, errorThrown) {
-		console.log('postUser error: ' + textStatus);
-	    }
+	$("#AjoutAdmin").click(function(){
+		$(".pRessource").hide();
+		$(".pExercice").hide();
+		$(".pEspace").hide();
+		$(".pAjoutAdmin").show();
+		$(".pAjoutSentiment").hide();
+		$(".pAjoutBesoin").hide();
+		$(".pAjoutExercice").hide();
+		$(".pAjoutQuestion").hide();
+		$(".pLogin").hide();
+		$(".pSign").hide();
 	});
-    }
 
-    $("#postBesoin").click(function(){
-	console.log($('#categorieBesoin').val())
-	console.log($('#nomBesoin').val())
-	postBesoin(
-	    $('#nomBesoin').val(),
-	    $('#categorieBesoin').val(),
-	    'http://localhost:8080/cye/besoin'
-	)});
-
-    function postBesoin(besoin,categorie,url) {
-	console.log("postSentiment " + url)
-	$.ajax({
-	    type : 'POST',
-	    contentType : 'application/json',
-	    url : url,
-	    dataType : "json",
-	    data : JSON.stringify({
-		"nom" : besoin,
-		"categorie" : categorie,
-		"id" : 0
-	    }),
-	    success : function(data, textStatus, jqXHR) {
-		//afficheUser(data);
-	    },
-	    error : function(jqXHR, textStatus, errorThrown) {
-		console.log('postUser error: ' + textStatus);
-	    }
+	$("#AjoutSentiment").click(function(){
+		$(".pRessource").hide();
+		$(".pExercice").hide();
+		$(".pEspace").hide();
+		$(".pAjoutAdmin").hide();
+		$(".pAjoutSentiment").show();
+		$(".pAjoutBesoin").hide();
+		$(".pAjoutExercice").hide();
+		$(".pAjoutQuestion").hide();
+		//$("#sentiment").hide();
+		$(".pLogin").hide();
+		$(".pSign").hide();
 	});
-    }
 
-    $("#postExercice").click(function(){
-	postExercice(
-	    $('#nomExercice').val(),
-	    $('#consigneGlobale').val(),
-	    $('#consigneQuestion').val(),
-	    'http://localhost:8080/cye/exercice'
-	)});
-
-    function postExercice(nom,consigneGlobale,consigneQuestion,url) {
-	console.log("postExercice " + url);
-	$.ajax({
-	    type : 'POST',
-	    contentType : 'application/json',
-	    url : url,
-	    dataType : "json",
-	    data : JSON.stringify({
-		"nom" : nom,
-		"cg" : consigneGlobale,
-		"cq" : consigneQuestion,
-		"id" : 0
-	    }),
-	    success : function(data, textStatus, jqXHR) {
-		//afficheUser(data);
-	    },
-	    error : function(jqXHR, textStatus, errorThrown) {
-		console.log('postUser error: ' + textStatus);
-	    }
+	$("#AjoutBesoin").click(function(){
+		$(".pRessource").hide();
+		$(".pExercice").hide();
+		$(".pEspace").hide();
+		$(".pAjoutAdmin").hide();
+		$(".pAjoutSentiment").hide();
+		$(".pAjoutBesoin").show();
+		$(".pAjoutExercice").hide();
+		$(".pAjoutQuestion").hide();
+		$(".pLogin").hide();
+		$(".pSign").hide();
 	});
-    }
 
-    $("#postQuestion").click(function(){
-	console.log("ICICI");
-	postQuestion(
-	    $('#idExo').val(),
-	    $('#phraseQuestion').val(),
-	    $('#correctionNon').val(),
-	    $('#correctionOui').val(),
-	    'http://localhost:8080/cye/question'
-	)});
-
-    function postQuestion(id,phraseQ,cNon, cOui,url) {
-	console.log("postQuestion " + url)
-	$.ajax({
-	    type : 'POST',
-	    contentType : 'application/json',
-	    url : url,
-	    dataType : "json",
-	    data : JSON.stringify({
-		"idExo" : id,
-		"question" : phraseQ,
-		"correctionNon" : cNon,
-		"correctionOui" : cOui,
-		"id" : 0
-	    }),
-	    success : function(data, textStatus, jqXHR) {
-		//afficheUser(data);
-	    },
-	    error : function(jqXHR, textStatus, errorThrown) {
-		console.log('postUser error: ' + textStatus);
-	    }
+	$("#AjoutExercice").click(function(){
+		$(".pRessource").hide();
+		$(".pExercice").hide();
+		$(".pEspace").hide();
+		$(".pAjoutAdmin").hide();
+		$(".pAjoutSentiment").hide();
+		$(".pAjoutBesoin").hide();
+		$(".pAjoutExercice").show();
+		$(".pAjoutQuestion").hide();
+		$(".pLogin").hide();
+		$(".pSign").hide();
 	});
-    }
 
+	$("#AjoutQuestion").click(function(){
+		$(".pRessource").hide();
+		$(".pExercice").hide();
+		$(".pEspace").hide();
+		$(".pAjoutAdmin").hide();
+		$(".pAjoutSentiment").hide();
+		$(".pAjoutBesoin").hide();
+		$(".pAjoutExercice").hide();
+		$(".pAjoutQuestion").show();
+		$(".pLogin").hide();
+		$(".pSign").hide();
+	});
 
-    $("#deleteSentiment").click(function(){
-	var chaine = 'http://localhost:8080/cye/sentiment/' + $('#idExo').val();
-	deleteSentiment(chaine);
-    });
+	$("#postSentiment").click(function(){
+		console.log($('#categorieSentiment').val())
+		console.log($('#nomSentiment').val())
+		postSentiment(
+			$('#nomSentiment').val(),
+			$('#categorieSentiment').val(),
+			'http://localhost:8080/cye/sentiment'
+		)});
 
-    function deleteSentiment(url){
-	$.ajax({
-	    type : 'DELETE',
-	    contentType : 'application/json',
-	    url : url,
-	    dataType : "json"
-	}),
-	success : function(data, textStatus, jqXHR) {
-	    //afficheUser(data);
-	},
-	error : function(jqXHR, textStatus, errorThrown) {
-	    console.log('postUser error: ' + textStatus);
+	function postSentiment(sentiment,categorie,url) {
+		console.log("postSentiment " + url);
+		$.ajax({
+			type : 'POST',
+			contentType : 'application/json',
+			url : url,
+			dataType : "json",
+			data : JSON.stringify({
+				"nom" : sentiment,
+				"categorie" : categorie,
+				"id" : 0
+			}),
+			success : function(data, textStatus, jqXHR) {
+				//afficheUser(data);
+			},
+			error : function(jqXHR, textStatus, errorThrown) {
+				console.log('postUser error: ' + textStatus);
+			}
+		});
 	}
-    }
+
+	$("#postBesoin").click(function(){
+		console.log($('#categorieBesoin').val())
+		console.log($('#nomBesoin').val())
+		postBesoin(
+			$('#nomBesoin').val(),
+			$('#categorieBesoin').val(),
+			'http://localhost:8080/cye/besoin'
+		)});
+
+	function postBesoin(besoin,categorie,url) {
+		console.log("postSentiment " + url)
+		$.ajax({
+			type : 'POST',
+			contentType : 'application/json',
+			url : url,
+			dataType : "json",
+			data : JSON.stringify({
+				"nom" : besoin,
+				"categorie" : categorie,
+				"id" : 0
+			}),
+			success : function(data, textStatus, jqXHR) {
+				//afficheUser(data);
+			},
+			error : function(jqXHR, textStatus, errorThrown) {
+				console.log('postUser error: ' + textStatus);
+			}
+		});
+	}
+
+	$("#postExercice").click(function(){
+		postExercice(
+			$('#nomExercice').val(),
+			$('#consigneGlobale').val(),
+			$('#consigneQuestion').val(),
+			'http://localhost:8080/cye/exercice'
+		)});
+
+	function postExercice(nom,consigneGlobale,consigneQuestion,url) {
+		console.log("postExercice " + url);
+		$.ajax({
+			type : 'POST',
+			contentType : 'application/json',
+			url : url,
+			dataType : "json",
+			data : JSON.stringify({
+				"nom" : nom,
+				"cg" : consigneGlobale,
+				"cq" : consigneQuestion,
+				"id" : 0
+			}),
+			success : function(data, textStatus, jqXHR) {
+				//afficheUser(data);
+			},
+			error : function(jqXHR, textStatus, errorThrown) {
+				console.log('postUser error: ' + textStatus);
+			}
+		});
+	}
+
+	$("#postQuestion").click(function(){
+		console.log("ICICI");
+		postQuestion(
+			$('#idExo').val(),
+			$('#phraseQuestion').val(),
+			$('#correctionNon').val(),
+			$('#correctionOui').val(),
+			'http://localhost:8080/cye/question'
+		)});
+
+	function postQuestion(id,phraseQ,cNon, cOui,url) {
+		console.log("postQuestion " + url)
+		$.ajax({
+			type : 'POST',
+			contentType : 'application/json',
+			url : url,
+			dataType : "json",
+			data : JSON.stringify({
+				"idExo" : id,
+				"question" : phraseQ,
+				"correctionNon" : cNon,
+				"correctionOui" : cOui,
+				"id" : 0
+			}),
+			success : function(data, textStatus, jqXHR) {
+				//afficheUser(data);
+			},
+			error : function(jqXHR, textStatus, errorThrown) {
+				console.log('postUser error: ' + textStatus);
+			}
+		});
+	}
+
+
+	$("#deleteSentiment").click(function(){
+		var chaine = 'http://localhost:8080/cye/sentiment/' + $('#idExo').val();
+		deleteSentiment(chaine);
+	});
+
+	function deleteSentiment(url){
+		$.ajax({
+			type : 'DELETE',
+			contentType : 'application/json',
+			url : url,
+			dataType : "json",
+			success : function(data, textStatus, jqXHR) {
+				//afficheUser(data);
+			},
+			error : function(jqXHR, textStatus, errorThrown) {
+				console.log('postUser error: ' + textStatus);
+			}});
+	}
 });
